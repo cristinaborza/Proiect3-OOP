155216f6f934422a4cc66a0afc8f04d4896e75dd
cristinaborza <44498369+cristinaborza@users.noreply.github.com>
2020-05-15 11:34:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,53 +2,81 @@
 
 #include "produs.h"
 #include "pizza.h"
-#include "pizzaVegetariana.h"
+#include "pizzaOnline.h"
 #include "ingredient.h"
 #include "meniu.h"
 #include "PizzaExistenta.h"
-#include "PizzaVegExistenta.h"
 #include "PizzaInexistenta.h"
-#include "PizzaVegInexistenta.h"
 
 using namespace std;
 
-template <> int Meniu<Pizza>::idx = 0;
-double Meniu<PizzaVegetariana>::valIncasari = 0;
+template <> int Meniu <Pizza>::idx = 0;
+int Meniu <PizzaOnline>::idx = 0;
 
 void afiseazaMeniu() {
     cout << "MENIU:\n";
-    cout << "1.Afiseaza meniul\n";
-    cout << "2.Adauga o pizza in meniu\n";
-    cout << "3.Comanda o pizza\n";
-    cout << "4.Afiseaza pretul total al pizzelor vegetariene comandate pana acum\n";
-    cout << "5.Exit\n";
+    cout << "1.Afiseaza meniul.\n";
+    cout << "2.Adauga o pizza in meniu.\n";
+    cout << "3.Comanda o pizza.\n";
+    cout << "4.Afiseaza incasari.\n";
+    cout << "5.Exit.\n";
 }
 
 int main() {
-    Meniu<Pizza> mp;
-    Meniu<PizzaVegetariana> mpv;
+    map <int, vector <Ingredient> > mp;
+    int n; cin >> n;
+    for (int i = 1; i <= n; ++ i) {
+        int type; cin >> type;
+        if (type == 1) {
+            cout << "Introduceti id-ul pizzei:\n";
+            int id; cin >> id;
+            if (mp.find(id) != mp.end()) {
+                cout << "Exista deja o pizza cu aces id!\n";
+                continue;
+            }
+
+            Pizza p;
+            cin >> p;
+            mp[id] = p.getIng();
+        } else {
+            cout << "Introduceti id-ul pizzei:\n";
+            int id; cin >> id;
+            if (mp.find(id) == mp.end()) cout << "Nu exista o pizza cu acest id!\n";
+            else {
+                cout << "Ingredientele sunt: ";
+                vector <Ingredient> v = mp[id];
+                for (auto it: v) {
+                    cout << it << " ";
+                }
+                cout << '\n';
+            }
+        }
+    }
+
+
+
+    Meniu<Pizza> meniuPizza;
+    Meniu<PizzaOnline> meniuPizzaOnline;
 
     while (true) {
         afiseazaMeniu();
 
         int type; cin >> type;
         if (type == 1) {
-            cout << "Ce fel de meniu doriti sa se afiseze? (1 - meniu pizza, 2 - meniu pizza vegetariana)";
+            cout << "Ce fel de meniu doriti sa se afiseze? (1 - meniu pizza, 2 - meniu pizza online)";
             int type; cin >> type;
-            if (type == 1) cout << mp << '\n';
-            else cout << mpv << '\n';
+            if (type == 1) cout << meniuPizza << '\n';
+            else cout << meniuPizzaOnline << '\n';
         }
 
         else if (type == 2) {
-            cout << "Introdu tipul produsului(1 - pizza, 2 - pizza vegetariana)\n";
+            cout << "In ce meniu vreti sa introduceti produsul? (1 - meniu pizza, 2 - meniu pizza online)\n";
             int type; cin >> type;
             if (type == 1) {
                 try {
                     Pizza p; cin >> p;
-                    if (mp.check(p.getNume())) throw (PizzaExistenta());
-
-                    mp += 1;
-                    mp.adauga(p);
+                    if (meniuPizza.check(p.getNume())) throw (PizzaExistenta());
+                    meniuPizza += p;
                 } catch (exception &e) {
                     cout << e.what() << '\n';
                 }
@@ -56,79 +84,48 @@ int main() {
 
             else {
                 try {
-                    PizzaVegetariana p; cin >> p;
-                    if (mpv.check(p.getNume())) throw (PizzaVegExistenta());
-                    mpv.adauga(p);
+                    PizzaOnline p; cin >> p;
+                    if (meniuPizzaOnline.check(p.getNume())) throw (PizzaExistenta());
+                    meniuPizzaOnline += p;
                 } catch (exception &e) {
                     cout << e.what() << '\n';
                 }
-
             }
         }
 
         else if (type == 3) {
-            cout << "Introdu tipul produsului(1 - pizza, 2 - pizza vegetariana)\n";
-            int tip; cin >> tip;
-            cout << "Introdu numele produsului pe care vrei sa il comanzi:\n";
-            string nume; cin >> nume;
             cout << "Introdu tipul de comanda(1 - Cu livrare, 2 - Cu ridicare)\n";
             int type; cin >> type;
+            cout << "Introduceti indexul produsului:\n";
+            int idx; cin >> idx;
 
-            if (tip == 1) {
-                if (type == 2) {
-                    try {
-                        if (!mp.check(nume)) throw (PizzaInexistenta());
-                        cout << "Costul este de " << mp.getPret(nume) << " lei.\n";
-                    } catch (exception &e) {
-                        cout << e.what() << '\n';
-                    }
-                }
-                else {
-                    cout << "Introdu distanta pana la destinatie:\n";
-                    int dist; cin >> dist;
-
-                    try {
-                        if (!mp.check(nume)) throw (PizzaInexistenta());
-                        double ans = (double) mp.getPret(nume);
-                        ans = ans + 0.05 * ans * (dist / 10);
-                        cout << "Costul este de " << ans << " lei.\n";
-                    } catch (exception &e) {
-                        cout << e.what() << '\n';
-                    }
+            if (type == 2) {
+                try {
+                    if (idx < 1 || idx > meniuPizza.getIdx()) throw (PizzaInexistenta());
+                    meniuPizza.vinde(idx);
+                } catch (exception &e) {
+                    cout << e.what() << '\n';
                 }
             }
 
             else {
-                if (type == 2) {
-                    try {
-                        if (!mpv.check(nume)) throw (PizzaVegInexistenta());
-                        int ans = mpv.getPret(nume);
-                        mpv += (double)ans;
-                        cout << "Costul este de " << ans << " lei.\n";
-                    } catch (exception &e) {
-                        cout << e.what() << '\n';
-                    }
-
-                }
-                else {
-                    cout << "Introdu distanta pana la destinatie:\n";
-                    int dist; cin >> dist;
-
-                    try {
-                        if (!mpv.check(nume)) throw (PizzaVegInexistenta());
-                        double ans = (double) mpv.getPret(nume);
-                        ans = ans + 0.05 * ans * (dist / 10);
-                        mpv += ans;
-                        cout << "Costul este de " << ans << " lei.\n";
-                    } catch (exception &e) {
-                        cout << e.what() << '\n';
-                    }
+                try {
+                    if (idx < 1 || idx > meniuPizzaOnline.getIdx()) throw (PizzaInexistenta());
+                    meniuPizzaOnline.vinde(idx);
+                } catch (exception &e) {
+                    cout << e.what() << '\n';
                 }
             }
         }
 
         else if (type == 4) {
-            cout << "Pretul total al pizzelor vegetariene este de: " << Meniu<PizzaVegetariana>::getValIncasari() << " lei\n";
+            cout << "Pentru ce meniu sa se afiseze incasarile: (1 - meniu, 2 - meniu online)?\n";
+            int type; cin >> type;
+            if (type == 1) cout << "Incasarile sunt de: " << meniuPizza.getIncasari() << " lei.\n";
+            else {
+                cout << "Incasarile sunt de: " << meniuPizzaOnline.getIncasari() << " lei.\n";
+                cout << "Incasarile pentru pizzele vegetariene sunt de " << meniuPizzaOnline.getIncasariVeg() << " lei.\n";
+            }
         }
 
         else if (type == 5) {
